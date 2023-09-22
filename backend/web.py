6089edf0dc1c4da802b66684db9b2fa7ac04a857
zushun.zhan <zushun.zhan@sumscope.com>
2023-09-22 16:48:57 +0800

    use different users for different asks

--- backend/web.py
+++ backend/web.py
@@ -128,14 +128,14 @@ async def ask_doc(request: Request):
 
     if (file_id_list is None) or len(file_id_list) == 0:
         logging.info('未选择文件，转发到普通对话')
-        return await proxy(user_name, query_str, 'gpt-3.5-turbo')
+        return await proxy(user_name + '.chat', query_str, 'gpt-3.5-turbo')
 
     prompt = """判断以下输入【问题】的类别，总共有两种类别，一种是【做总结】，另一种是【普通问答】，
 只有明显中文或英文提到“总结”“小结”时才是【做总结】，如“业务回顾”属于普通问答。你只需要输出最终答案，无需给出分析过程，最终答案采用json格式返回，
 格式为 {"类别":"做总结"} 或者 {"类别":"普通问答"}，【问题】如下【问题】如下:"""
 
     prompt = prompt + query_str
-    question_type = await proxy_sync(user_name, prompt, 'gpt-4')
+    question_type = await proxy_sync(user_name + ".judge", prompt, 'gpt-4')
     logging.info(f'question_type={question_type}')
     if ('做总结' in question_type):
         logging.info('问题是总结类')
@@ -182,7 +182,7 @@ async def chat(request: Request):
     prompt = data.get('prompt')
     user_name = data.get('user')
 
-    return await proxy(user_name, prompt, 'gpt-4')
+    return await proxy(user_name + '.chat', prompt, 'gpt-4')
 
 
 
