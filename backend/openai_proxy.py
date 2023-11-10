187a0a86f7f1fbae888b305442d369b7e9de6f0f
zhanzushun <zhanzushun@hotmail.com>
2023-11-10 11:25:50 +0800

    logging

--- backend/openai_proxy.py
+++ backend/openai_proxy.py
@@ -14,8 +14,17 @@ async def proxy_generator(user: str, prompt: str, model: str):
     async with aiohttp.ClientSession() as session:
         try:
             async with session.post(URL, json={"prompt":prompt, "user": user, "user_group": "marvin", "model": model}) as response:
+                s = ''
                 async for data in response.content.iter_any():
                     yield data
+                    try:
+                        s += str(data.decode('utf-8'))
+                    except:
+                        logging.exception('ops')
+                        
+                logging.info('========start reply========')
+                logging.info(s)
+                logging.info('========end of reply========')
         except Exception as e:
             yield f'Exception: {e}'
 
